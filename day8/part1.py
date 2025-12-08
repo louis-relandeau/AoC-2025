de2a1db2ee7ec9dd99a1f4f40c44433ad797e676
louis relandeau <nonmerci77@gmail.com>
2025-12-08 22:08:41 +0100

    day8 it's not always about pretty sometimes it's about working

---
+++ day8/part1.py
@@ -0,0 +1,40 @@
+with open("input.txt") as f:
+    coords = [tuple([int(c) for c in coord.strip().split(',')]) for coord in f.readlines()]
+
+distances = []
+for i in range(len(coords)):
+    for j in range(i+1, len(coords), 1):
+        a1, b1, c1 = coords[i]
+        a2, b2, c2 = coords[j]
+        dist = ((a2-a1)**2+(b2-b1)**2+(c2-c1)**2)**0.5
+        distances.append((dist, coords[i], coords[j]))
+distances.sort(key=lambda x: x[0])
+
+assigned_nodes = {}
+circuit_index = 0
+for i in range(1000):
+    d1, d2 = distances[i][1:]
+    circuit1 = assigned_nodes.get(d1, None)
+    circuit2 = assigned_nodes.get(d2, None)
+    if circuit1 and not circuit2:
+        assigned_nodes[d2] = circuit1
+    elif circuit2 and not circuit1:
+        assigned_nodes[d1] = circuit2
+    elif not circuit1 and not circuit2:
+        assigned_nodes[d1] = circuit_index
+        assigned_nodes[d2] = circuit_index
+        circuit_index += 1
+    elif circuit1 != circuit2:
+        for n, c in assigned_nodes.items():
+            if c == circuit2:
+                assigned_nodes[n] = circuit1
+
+circuit_counts = {}
+for c in assigned_nodes.values():
+    circuit_counts[c] = circuit_counts.get(c, 0) + 1
+
+counts = sorted(circuit_counts.values(), reverse=True)
+res = 1
+for val in counts[:3]:
+    res *= val
+print(res)
